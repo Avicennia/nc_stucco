aabfbc80cb6f2a17c392ba21243c6ed7604ffaf2
Seth Hunter <a.germinans@gmail.com>
2020-05-28 19:19:53 -0700

    nc_concrete compatibility changes
    - A few changes to support earlier users while continuing to phase out legacy code in lieu of nc_concrete's accomodations.
    - Node registrations for legacy stucco nodes removed.
    - Aliasing and checks to ensure that older nodes are not dropped, but rather upgraded to nc_concrete variants.
    - Support for those still playing older versions of Nodecore is currently in progress.
    - Avi

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ local dofile, minetest, rawset
 
 local thismod = minetest.get_current_modname()
 local modpath = minetest.get_modpath(thismod)
+parentmod = minetest.get_modpath("nc_concrete")
 
 nc_stuccol = {
 	curing = {
@@ -20,16 +21,41 @@ nc_stuccol = {
 			damp = "dry"
 		}
 	},
-	patterns = {"GKey", "Beetle", "Bindy", "Crossy", "Corinth", "Hashy",
-		"Icebox", "Vermi", "Bricky", "Hexy", "Brexy", "Barry", "Ridgey",
+	patterns = {"GKey", "Beetle", "Crossy", "Corinth", "Hexy", "Brexy",
 		"Lamby", "Doric", "Starry", "Targey", "Stakey", "Panelly",
 		"Slatty", "Logos", "Peeky", "Mucky", "Enol", "SN", "Hatchy",
-		"Bordy-Thin", "Bordy-Thick", "Foote"},
+		"Bordywalk", "Bordyrun", "Foote"},
+	wca = {"Bindy", "Bricky", "Hashy","Icebox","Ridgey", "Vermi", "Barry"},
+	dep = {"Bordy-Thin", "Bordy-Thick"},
 	posts = {"Thick", "Thin"},
 }
+
+
+
+nms = {
+	mod = "nc_concrete",
+	na = {"sandstone", "adobe", "coalstone"}
+}
+
 rawset(_G, thismod, nc_stuccol)
 
-dofile(modpath .. "/curing.lua")
-dofile(modpath .. "/crafting.lua")
+
 dofile(modpath .. "/nodes.lua")
-dofile(modpath .. "/particles.lua")
+
+
+minetest.after(1, function()
+for k,_ in pairs(minetest.registered_nodes) do
+	if(string.find(k,"nc_concrete:concrete_") and not string.find(k,"ply"))then
+		local ngroups = minetest.registered_nodes[k]["groups"]
+		if(string.find(k, "ad"))then
+		ngroups.nc_stucco_etched_hard_adobe = 1
+		minetest.override_item(k, {groups = ngroups})
+		elseif(string.find(k, "co"))then
+		ngroups.nc_stucco_etched_hard_coalstone = 1
+		minetest.override_item(k, {groups = ngroups})
+		else end
+	else end
+
+end
+end)
+
--- nodes.lua
+++ nodes.lua
@@ -9,149 +9,113 @@ local ItemStack, minetest, nodecore, pairs, tostring
 -- Wet and Dry versions of patterned nodes.
 
 local function lc(n) return tostring(n):lower():gsub("%W", "") end
-
-local function stylus_change(pos, player, idx, type)
-	local wield = player:get_wielded_item()
-	if wield:get_name() ~= "nc_stucco:stylusW" then return end
-	if idx < 1 then idx = #nc_stuccol.patterns end
-	if idx > #nc_stuccol.patterns then idx = 1 end
-	minetest.set_node(pos, {name = "nc_stucco:stucco_"
-			.. lc(nc_stuccol.patterns[idx]) .. "_" .. type})
-	nodecore.show_touchtip(player, nodecore.touchtip_node(pos))
-	wield:add_wear(65536 / 250)
-	if wield:get_count() == 0 then
-		minetest.sound_play("nc_api_toolbreak",
-			{object = player, gain = 0.5})
-		wield = ItemStack("nc_tree:stick")
-	end
-	player:set_wielded_item(wield)
+local function cl(n)
+    n = n:lower()
+    n = (n == "vermi" and "vermy") or (n == "barry" and "verty") or (n == "ridgey" and "horzy") or (n == "icebox" and "iceboxy") or n
+    return n
 end
+
+minetest.register_alias("nc_stucco:stylusW", "nc_concrete:stylus")
+
+nc_stuccol.register_stucco = function(pat, mat, iswet)
+    iswet = iswet and "_wet" or ""
+    local isply = iswet and "_ply" or ""
+    local nmat = mat == "clay" and "adobe" or mat == "slate" and "coalstone"
+    local oldname,newname = "nc_stucco:stucco_"..pat.."_"..mat..iswet, "nc_concrete:concrete_"..lc(nmat)..isply
+    return minetest.register_node(oldname, {}), minetest.register_alias_force(newname, oldname)
+end
+
+
+-- Refashioning legacy nodes into new, stronger, concrete versions
+local gentrify_masonry = function()
+local mats = {"clay","slate"}
+
+for n = 1, 2, 1 do
+local mat = mats[n]
+
+for n = 1, #nc_stuccol.patterns, 1 do
+    local v = nc_stuccol.patterns[n]
+    nc_stuccol.register_stucco(v, mat, true)
+    nc_stuccol.register_stucco(v, mat, false)
+end
+
+
+end
+
+if(minetest.registered_nodes[nc_stuccol.dep[1]])then
+    minetest.register_alias_force("nc_stucco:stucco_bordywalk_clay", "nc_stucco:stucco_Bordy-Thin_clay")
+    minetest.register_alias_force("nc_stucco:stucco_bordyrun_clay", "nc_stucco:stucco_Bordy-Thick_clay")
+    minetest.register_alias_force("nc_stucco:stucco_bordywalk_clay_wet", "nc_stucco:stucco_Bordy-Thin_clay_wet")
+    minetest.register_alias_force("nc_stucco:stucco_bordyrun_clay_wet", "nc_stucco:stucco_Bordy-Thick_clay_wet")
+    minetest.register_alias_force("nc_stucco:stucco_bordywalk_slate", "nc_stucco:stucco_Bordy-Thin_slate")
+    minetest.register_alias_force("nc_stucco:stucco_bordyrun_slate_wet", "nc_stucco:stucco_Bordy-Thick_slate_wet")
+    minetest.register_alias_force("nc_stucco:stucco_bordywalk_slate_wet", "nc_stucco:stucco_Bordy-Thin_slate_wet")
+    minetest.register_alias_force("nc_stucco:stucco_bordyrun_slate", "nc_stucco:stucco_Bordy-Thick_slate")
+else end
+
+end
+gentrify_masonry()
 
 --Clay
-for k, v in pairs(nc_stuccol.patterns) do
-	minetest.register_node("nc_stucco:stucco_" .. lc(v) .. "_clay", {
-			description = "Wet Clay Stucco with " .. v .. " Pattern",
-			tiles = {"nc_stucco_canvas_1a.png^nc_stucco_pattern_" .. lc(v) .. ".png"},
-			groups = {workable = 1, crumbly = 2, stucco_damp = 1},
-			on_punch = function(pos, node, puncher, pointed_thing)
-				minetest.node_punch(pos, node, puncher, pointed_thing)
-				return stylus_change(pos, puncher, k + 1, "clay")
-			end,
-			on_rightclick = function(pos, _, clicker)
-				return stylus_change(pos, clicker, k - 1, "clay")
-			end,
-			stucco_wetter = "nc_stucco:stucco_sodden_clay",
-			stucco_drier = "nc_stucco:stucco_" .. lc(v) .. "_clay_dry",
-			sounds = nodecore.sounds("nc_stucco_chulky")
-		})
-	minetest.register_node("nc_stucco:stucco_" .. lc(v) .. "_clay_dry", {
-			description = "Dry Clay Stucco with " .. v .. " Pattern",
-			tiles = {"nc_stucco_canvas_1b.png^nc_stucco_pattern_" .. lc(v) .. ".png"},
-			groups = {cracky = 2, stucco_clay_dry = 1},
-			sounds = nodecore.sounds("nc_stucco_hard")
-		})
-	-- Slate
-	minetest.register_node("nc_stucco:stucco_" .. lc(v) .. "_slate", {
-			description = "Wet Slate Stucco with " .. v .. " Pattern",
-			tiles = {"nc_stucco_canvas_2.png^nc_stucco_pattern_"
-				.. lc(v) .. ".png^nc_stucco_pattern_" .. lc(v) .. ".png"},
-			groups = {workable = 1, crumbly = 2},
-			on_punch = function(pos, node, puncher, pointed_thing)
-				minetest.node_punch(pos, node, puncher, pointed_thing)
-				return stylus_change(pos, puncher, k + 1, "slate")
-			end,
-			on_rightclick = function(pos, _, clicker)
-				return stylus_change(pos, clicker, k - 1, "slate")
-			end,
-			stucco_wetter = "nc_stucco:stucco_sodden_slate",
-			stucco_drier = "nc_stucco:stucco_" .. lc(v) .. "_clay_slate", sounds = nodecore.sounds("nc_stucco_chulky")
-		})
-	minetest.register_node("nc_stucco:stucco_" .. lc(v) .. "_slate_dry", {
-			description = "Dry Slate Stucco with " .. v .. " Pattern",
-			tiles = {"nc_stucco_canvas_2.png^nc_stucco_pattern_" .. lc(v) .. ".png"},
-			groups = {cracky = 2, stucco_slate_dry = 1},
-			sounds = nodecore.sounds("nc_stucco_hard")
-		})
+for n = 1, #nc_stuccol.wca, 1 do
+    local v = nc_stuccol.wca[n]
+    minetest.register_alias_force("nc_stucco:stucco_" .. lc(v) .. "_clay", "nc_concrete:concrete_adobe_"..cl(v).."_ply")
+    minetest.register_alias_force("nc_stucco:stucco_" .. lc(v) .. "_clay_dry", "nc_concrete:concrete_adobe_"..cl(v))
+
+
+    -- Slate
+    minetest.register_alias_force("nc_stucco:stucco_" .. lc(v) .. "_slate", "nc_concrete:concrete_coalstone_"..cl(v).."_ply")
+    minetest.register_alias_force("nc_stucco:stucco_" .. lc(v) .. "_slate_dry", "nc_concrete:concrete_coalstone_"..cl(v))
+
+
 end
+
+for n = 1, #nc_stuccol.patterns, 1 do
+    local v = nc_stuccol.patterns[n]
+    nodecore.register_concrete_pattern({description = lc(v)})
+end
+
 -- Curing Stage Nodes.
-local function nextstage(lut, v, material)
-	local x = nc_stuccol.curing[lut][lc(v)]
-	return x and ("nc_stucco:stucco_" .. x .. "_" .. material)
-end
-for _, v in pairs(nc_stuccol.curing.stages) do
-	minetest.register_node("nc_stucco:stucco_" .. lc(v) .. "_clay", {
-			description = v .. " Clay Stucco",
-			tiles = {"nc_stucco_canvas_1a.png^nc_stucco_curing_" .. lc(v) .. ".png"},
-			groups = {crumbly = 2, stucco_damp = 1, falling_node = 1},
-			sounds = nodecore.sounds("nc_stucco_chulky"),
-			stucco_wetter = nextstage("wetter", v, "clay"),
-			stucco_drier = nextstage("drier", v, "clay")
-		})
-	minetest.register_node("nc_stucco:stucco_" .. lc(v) .. "_slate", {
-			description = v .. " Slate Stucco",
-			tiles = {"nc_stucco_canvas_2.png^nc_stucco_curing_" .. lc(v) .. ".png"},
-			groups = {crumbly = 2, stucco_damp = 1, falling_node = 1},
-			sounds = nodecore.sounds("nc_stucco_chulky"),
-			stucco_wetter = nextstage("wetter", v, "slate"),
-			stucco_drier = nextstage("drier", v, "slate")
-		})
+for n = 1, #nc_stuccol.curing.stages, 1 do
+    local v = nc_stuccol.curing.stages[n]
+    minetest.register_alias("nc_stucco:stucco_" .. lc(v) .. "_clay","nc_concrete:concrete_adobe_".."blank".."_ply")
+    minetest.register_alias("nc_stucco:stucco_" .. "powdered" .. "_clay","nc_concrete:concrete_adobe_".."blank".."_ply")
+
+    minetest.register_alias("nc_stucco:stucco_" .. lc(v) .. "_slate","nc_concrete:concrete_coalstone_".."blank".."_ply")
+    minetest.register_alias("nc_stucco:stucco_" .. "powdered" .. "_slate","nc_concrete:concrete_coalstone_".."blank".."_ply")
+
 end
 -- Crushed Nodes.
-minetest.register_node("nc_stucco:crushed_stucco_clay", {
-		description = "Crushed Clay Stucco",
-		tiles = {"nc_stucco_canvas_1a.png^[mask:nc_stucco_pattern_vermi.png"},
-		groups = {crumbly = 1, falling_node = 1},
-		sounds = nodecore.sounds("nc_stucco_hard")
-	})
+local namepool = {}
+for k,v in pairs(minetest.registered_nodes) do
+    if(string.find(k,"nc_concrete:concrete_") and not string.find(k,"ply"))then
+        table.insert(namepool, k)
+    else end
 
-minetest.register_node("nc_stucco:crushed_stucco_slate", {
-		description = "Crushed Slate Stucco",
-		tiles = {"nc_stucco_canvas_2.png^[mask:nc_stucco_pattern_vermi.png"},
-		groups = {crumbly = 1, falling_node = 1},
-		sounds = nodecore.sounds("nc_stucco_hard")
-	})
--- Post Nodes.
-for n=1, #nc_stuccol.posts, 1 do
-	minetest.register_node("nc_stucco:post_" .. lc(n) .. "_slate", {
-			description = "Slate Stucco Post with " .. lc(nc_stuccol.posts[n]) .. " Border",
-			tiles = {"nc_stucco_canvas_2.png^nc_stucco_post_bordy"
-			.. lc(nc_stuccol.posts[n]) .. ".png"},
-			groups = {cracky = 3},
-			drawtype = "nodebox",
-			paramtype = "light",
-			node_box = {
-				type = "connected",
-				fixed = {
-					{-0.125, -0.5, -0.125, 0.125, 0.5, 0.125}
-				},
-				connect_front = {{-0.125, -0.5, -0.5, 0.125, 0.0625, -0.125}},
-				connect_back = {{-0.125, -0.5, 0.125, 0.125, 0.0625, 0.5}},
-				connect_left = {{-0.5, -0.5, -0.125, -0.125, 0.0625, 0.125}},
-				connect_right = {{0.125, -0.5, -0.125, 0.5, 0.0625, 0.125}}
-			},
-			connects_to = {"group:cracky"},
-			connect_sides = {"top", "bottom", "front", "left", "back", "right"},
-			sounds = nodecore.sounds("nc_stucco_hard")
-		})
-	minetest.register_node("nc_stucco:post_" .. lc(n) .. "_clay", {
-			description = "Clay Stucco Post with " .. lc(nc_stuccol.posts[n]) .. " Border",
-			tiles = {"nc_stucco_canvas_1a.png^nc_stucco_post_bordy"
-			.. lc(lc(nc_stuccol.posts[n])) .. ".png"},
-			groups = {cracky = 2},
-			drawtype = "nodebox",
-			paramtype = "light",
-			node_box = {
-				type = "connected",
-				fixed = {
-					{-0.125, -0.5, -0.125, 0.125, 0.5, 0.125}
-				},
-				connect_front = {{-0.125, -0.5, -0.5, 0.125, 0.0625, -0.125}},
-				connect_back = {{-0.125, -0.5, 0.125, 0.125, 0.0625, 0.5}},
-				connect_left = {{-0.5, -0.5, -0.125, -0.125, 0.0625, 0.125}},
-				connect_right = {{0.125, -0.5, -0.125, 0.5, 0.0625, 0.125}}
-			},
-			connects_to = {"group:cracky"},
-			connect_sides = {"top", "bottom", "front", "left", "back", "right"},
-			sounds = nodecore.sounds("nc_stucco_hard")
-		})
 end
+nodecore.register_craft({
+    label = "crush dry concrete",
+    action = "pummel",
+    priority = 1,
+    toolgroups = {thumpy = 2},
+    duration = 0.3,
+    nodes = {
+        {
+            match = { groups = {nc_stucco_etched_hard_adobe = true}},
+            replace = "nc_concrete:mud"
+        },
+    }
+})
+nodecore.register_craft({
+    label = "crush dry concrete",
+    action = "pummel",
+    priority = 1,
+    toolgroups = {thumpy = 2},
+    duration = 0.3,
+    nodes = {
+        {
+            match = { groups = {nc_stucco_etched_hard_coalstone = true}},
+            replace = "nc_concrete:render"
+        },
+    }
+})
